21d124f9ab269da3ad2a3e0b6601d0d0c88609ed
Valium-K <lschan96@gmail.com>
2022-01-12 16:27:27 +0900

    카카오 코딩테스트

--- dev.valium.algorithm/kakao/tet.py
+++ dev.valium.algorithm/kakao/tet.py
@@ -1,20 +1,26 @@
-import heapq
-
-def solution(n, times):
-    desk_arr = [[0, time] for time in times]
-    heapq.heapify(desk_arr)
-
-    while n > 0:
-        target_desk = desk_arr[0]
-
-        for i in range(len(desk_arr)):
-            if desk_arr[i][0] + desk_arr[i][1] < desk_arr[0][0] + desk_arr[0][1]:
-                target_desk = desk_arr[i]
-
-        target_desk[0] += target_desk[1]
-        n -= 1
-       # heapq.heapify(desk_arr)
-
-    return max(desk[0] for desk in desk_arr)
+from collections import Counter
+import math
+import time
+from collections import deque
+def gcd(a, b):
+    if b == 0:
+        print(a, b)
+        return a
+    else:
+        print(a, b)
+        return gcd(b, a % b)
 if __name__ == '__main__':
-    print(solution(6 ,	[7, 10]))
+    # 3
+    # WatanabE
+    # ITO
+    # YamaMoto
+
+    n = int(input())
+
+    for _ in range(n):
+        for c in input():
+            if ord('A') <= ord(c) <= ord('Z'):
+                print(chr(ord(c) + 32), end='')
+            else:
+                print(c, end='')
+        print()
